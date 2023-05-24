f5f1c62f342b69ccb11e993b9838357d945a83f5
Kleber Molina Kardel <molinakardel@gmail.com>
2023-05-24 15:52:40 -0300

    Instala e importa biblioteca de ícones

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
+import {} from 'react-icons/fi'
+
 function App() {
   return (
-    <div className="App">
-      
+    <div className="container">
+      <h1 className="title">Buscador CEP</h1>
+
+      <div className="containerInput">
+        <input
+        type="text"
+        placeholder="Digite seu CEP..."
+        />
+
+        <button className="buttonSearch">
+          Buscar
+        </button>
+      </div>
     </div>
   );
 }
